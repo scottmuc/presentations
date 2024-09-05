9961f24f5487b0e2f47216a0c426918645e64ede
ninabohm <boehm@sell-pick.com>
2024-09-05 10:48:58 +0200

    Begin workshop scenario testing

--- git-workshop/features/steps/tutorial.py
+++ git-workshop/features/steps/tutorial.py
@@ -11,3 +11,18 @@ def step_impl(context):
 @then('behave will test it for us!')
 def step_impl(context):
     assert context.failed is False
+
+
+@given(u'there was a commit with a commit message that is "foo"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given there was a commit with a commit message that is "foo"')
+
+
+@when(u'we run the log')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we run the log')
+
+
+@then(u'the log shows an entry with the message that is "foo"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the log shows an entry with the message that is "foo"')
--- git-workshop/features/tutorial.feature
+++ git-workshop/features/tutorial.feature
@@ -1,6 +1,11 @@
 Feature: showing off behave
 
   Scenario: run a simple test
-     Given we have behave installed
-      When we implement a test
-      Then behave will test it for us!
+    Given we have behave installed
+    When we implement a test
+    Then behave will test it for us!
+
+  Scenario: commit something
+    Given there was a commit with a commit message that is "foo"
+    When we run the log
+    Then the log shows an entry with the message that is "foo"
