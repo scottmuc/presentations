35a0626a2fee6a080b3a956fbabd5e93f3c3df8d
ninabohm <boehm@sell-pick.com>
2024-09-17 23:53:27 +0200

    Run the log and capture the output to assert it

--- git-workshop/features/steps/tutorial.py
+++ git-workshop/features/steps/tutorial.py
@@ -2,6 +2,7 @@ from behave import *
 import tempfile
 import shutil
 import subprocess
+import re
 
 @given('we have behave installed')
 def step_impl(context):
@@ -30,17 +31,33 @@ def step_impl(context):
     p = subprocess.Popen(['touch', 'foo'], cwd=dirpath)
     p.wait()
 
-    p = subprocess.Popen(['git', 'commit', '-am', 'foo'], cwd=dirpath)
+    p = subprocess.Popen(['git', 'add', './foo'], cwd=dirpath)
+    p.wait()
+
+    p = subprocess.Popen(['git', 'commit', '-m', 'foo'], cwd=dirpath)
     p.wait()
 
     print(dirpath)
 
+    # we need to make dirpath accessible to the @when and @then steps
+    context.dirpath = dirpath
+
 
 @when(u'we run the log')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When we run the log')
+    p = subprocess.Popen(['git', 'log', '--oneline', '--graph', '--all', '--decorate'], stdout=subprocess.PIPE, 
+                         stderr=subprocess.PIPE, 
+                         cwd=context.dirpath, 
+                         text=True) 
+    
+    # we need to obtain the output of the git log command
+    stdout, stderr = p.communicate()
+    context.log_output = stdout
+    print('context.log_output', context.log_output)
+    print('stdout', stdout)
+    
 
 
 @then(u'the log shows an entry with the message that is "foo"')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the log shows an entry with the message that is "foo"')
+    assert 'foo' in context.log_output
