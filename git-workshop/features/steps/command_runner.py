ed7ec6c43597969e9094af4371fdfb400e243e93
ninabohm <boehm@sell-pick.com>
2025-01-29 17:04:10 +0100

    Remove unneccessary run method

--- git-workshop/features/steps/command_runner.py
+++ git-workshop/features/steps/command_runner.py
@@ -2,12 +2,9 @@ import subprocess
 
 
 class CommandRunner:
-    def run(self, dirpath, command, *args):
+    def capture_output_from_command(self, dirpath, command, *args) -> str:
         self.subprocess = subprocess.Popen([command, *args], cwd=dirpath, stdout=subprocess.PIPE, text=True)
         self.subprocess.wait()
-
-    def capture_output_from_command(self, dirpath, command, *args) -> str:
-        self.run(dirpath, command, *args)
         stdout, stderr = self.subprocess.communicate()
         log_output = stdout.strip()
         if self.subprocess.returncode != 0:
--- git-workshop/features/steps/git_repo.py
+++ git-workshop/features/steps/git_repo.py
@@ -2,6 +2,7 @@ import tempfile
 
 from command_runner import CommandRunner
 
+
 class TempGitRepo:
     def __init__(self, dirpath=None, initial_branch="main"):
         if dirpath is None:
@@ -12,12 +13,12 @@ class TempGitRepo:
         self.cmd = CommandRunner()
 
     def init(self):
-        self.cmd.run(self.dirpath, 'git', 'init')
+        self.cmd.capture_output_from_command(self.dirpath, 'git', 'init')
 
     def add_test_commit_with_message(self, message):
-        self.cmd.run(self.dirpath, 'touch', message)
-        self.cmd.run(self.dirpath, 'git', 'add', message)
-        self.cmd.run(self.dirpath, 'git', 'commit', '-m', message)
+        self.cmd.capture_output_from_command(self.dirpath, 'touch', message)
+        self.cmd.capture_output_from_command(self.dirpath, 'git', 'add', message)
+        self.cmd.capture_output_from_command(self.dirpath, 'git', 'commit', '-m', message)
         
     def init_with_commits(self, messages):
         self.init()
@@ -25,7 +26,7 @@ class TempGitRepo:
             self.add_test_commit_with_message(message)
 
     def checkout_quiet(self, ref):
-        self.cmd.run(self.dirpath, 'git', 'checkout', '-q', ref)
+        self.cmd.capture_output_from_command(self.dirpath, 'git', 'checkout', '-q', ref)
     
     def read_head(self):
             return self.cmd.capture_output_from_command(self.dirpath, 'cat', '.git/HEAD')
