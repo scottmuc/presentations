58f2d3a444c4bca96628b609b47ca4a597d6277d
Scott Muc <scott@scottmuc.com>
2025-02-05 22:49:10 +0100

    Compare Commit SHAs of the specified branches
    Branchs point to SHAs and only HEAD can point to a name ref.

--- git-workshop/features/steps/creating_a_branch.py
+++ git-workshop/features/steps/creating_a_branch.py
@@ -12,10 +12,6 @@ def step_impl(context, branch_name):
 @then(u'branch {branch_1} and {branch_2} both point to the same commit')
 def step_impl(context, branch_1, branch_2):
     cmd = CommandRunner()
-    head_1 = cmd.run(context.repo.dirpath, 'cat', '.git/refs/heads/{branch_1}').output
-    print("head_1", head_1)
-    head_2 = cmd.run(context.repo.dirpath, 'cat', '.git/refs/heads/{branch_2}').output
-    expected_head_1 = 'ref: refs/heads/{branch_1}'
-    expected_head_2 = 'ref: refs/heads/{branch_2}'
-    assert head_1 == f"ref: refs/heads/{branch_1}", f"Expected {head_1} to be {expected_head_1}"
-    assert head_2 == f"ref: refs/heads/{branch_2}", f"Expected {head_2} to be {expected_head_2}"
+    head_1 = cmd.run(context.repo.dirpath, 'cat', f".git/refs/heads/{branch_1}").output
+    head_2 = cmd.run(context.repo.dirpath, 'cat', f".git/refs/heads/{branch_2}").output
+    assert head_1 == head_2
